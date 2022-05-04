1c968822fd8ee2570f899804e1db9c2653976b31
CJ Virtucio <cjv287@gmail.com>
2022-05-03 22:19:31 -0400

    added basic illustration of parent cancel cancelling children

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,41 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"time"
 )
 
+func fooBar(ctx context.Context, tries int) {
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	for i := tries; i >= 0; i-- {
+		printMsg("fooBar", "foo!", tries - i)
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func helloWorld(ctx context.Context, tries int) {
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	for i := tries; i >= 0; i-- {
+		printMsg("helloWorld", "hello!", tries - i)
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func printMsg(src string, msg string, num int) {
+	fmt.Printf("[%s | %d] %s\n", src, num, msg)
+}
+
 func main() {
-	fmt.Println("Hello world")
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go fooBar(ctx, 20)
+	go helloWorld(ctx, 20)
+
+	time.Sleep(5 * time.Second)
 }
